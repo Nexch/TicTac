ade020e38d025e49bd12b134a0700899e9fd5203
Nexch <mauricio.t.1999@gmail.com>
2020-09-30 09:24:12 -0400

    added the logic for tic tac toe

--- bin/main.rb
+++ bin/main.rb
@@ -46,13 +46,13 @@ class Game
   @@turn = 1
   def turns
     p 'Firt put the a number between 1-3 to select the row then put a number between 1-3 to select the column'
-    while @@turn < 9
-      flag = if @@turn.odd?
+    while @@turn < 10
+      @@flag = if @@turn.odd?
                'x'
              else
                'o'
              end
-      p "player #{flag}"
+      p "player #{@@flag}"
       p 'Select row'
       inp1 = gets
       p 'Select colum'
@@ -65,11 +65,17 @@ class Game
       inp2_i = (inp2_i * 6) - 3
       same = Logic.new
       same.same_place(inp1_i, inp2_i)
-      $arr[inp1_i][inp2_i] = flag
+      $arr[inp1_i][inp2_i] = @@flag
       board_1 = Board.new
       board_1.board
+      cheking = Logic.new
+      if @@turn >= 5
+        cheking.check
+      end
       @@turn += 1
     end
+    p 'draw'
+    play
   end
 
   def parameters(arg)
@@ -80,31 +86,65 @@ class Game
       turns
     end
   end
+
+  def win
+    p "Player #{@@flag} win"
+    play
+  end
+
+  def play
+    @@turn = 1
+    $arr = [[' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ']]
+    start
+  end
 end
 
 class Logic
-    def same_place(x, y)	   
-        if $arr[x][y] != " "	        
-            puts "You are cheating"	            
-            turn_again = Game.new	            
-            turn_again.turns	          
-        else 	       
-            puts "nice" 	           
-        end	        
-    end	    
-
-    def check
-    
+  def same_place(x, y)
+    if $arr[x][y] != " "
+      puts "You are cheating"
+      turn_again = Game.new
+      turn_again.turns
+    else
+      puts "nice"
     end
-end	
+  end
+
+  def check
+    winning = Game.new
+    if $arr[1][9] == $arr[0][15] && $arr[1][9] == $arr[2][3]
+      winning.win
+    elsif $arr[1][9] == $arr[0][3] && $arr[1][9] == $arr[2][15]
+      winning.win
+    elsif $arr[1][9] == $arr[0][9] && $arr[1][9] == $arr[2][9]
+      winning.win
+    elsif $arr[1][9] == $arr[1][15] && $arr[1][9] == $arr[1][3]
+      winnig.win
+    elsif $arr[0][3] == $arr[0][9] && $arr[0][3] == $arr[0][15]
+      winning.win
+    elsif $arr[0][3] == $arr[1][3] && $arr[0][3] == $arr[2][3]
+      winnig.win
+    elsif $arr[2][15] == $arr[2][9] && $arr[2][15] == $arr[2][3]
+      winning.win
+    elsif $arr[2][15] == $arr[1][15] && $arr[2][15] == $arr[0][15]
+      winning.win
+    end
+  end
+end
 
 puts 'TIC-TAC-TOE GAME'
-puts 'Do you want to play? (yes/no)'
 
-want_to_play = gets.chomp.downcase
-if want_to_play == 'yes' || want_to_play == 'y'
-  game_1 = Game.new
-  game_1.turns
-else
-  puts 'THANK YOU, GOOD LUCK!!'
+def start
+  puts 'Do you want to play? (yes/no)'
+
+  want_to_play = gets.chomp.downcase
+  if want_to_play == 'yes' || want_to_play == 'y'
+    game_1 = Game.new
+    game_1.turns
+  else
+    puts 'THANK YOU, GOOD LUCK!!'
+    exit
+  end
 end
+
+start
