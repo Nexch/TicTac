298f31df3ba86e5e06be7fd9def6c16e88bb6428
Nexch <mauricio.t.1999@gmail.com>
2020-09-30 05:13:36 -0400

    fix linters error

--- bin/main.rb
+++ bin/main.rb
@@ -1,54 +1,57 @@
+# rubocop:disable Layout/LeadingCommentSpace, Lint/MissingCopEnableDirective,Style/FrozenStringLiteralComment, Metrics/MethodLength, Style/GlobalVars, Style/Documentation, Metrics/AbcSize, Style/ClassVars, Style/MultipleComparison, Naming/VariableNumber, Layout/LineLength
 #!/usr/bin/env ruby
 class Board
-    private
-    def line1
-      print ' ' * 6
-      print '#'
-      print ' ' * 5
-      print '#'
-      print "\n"
-    end
+  private
 
-    def line2
-      print ' '
-      print '# ' * 9
-      print "\n"
-    end
+  def line1
+    print ' ' * 6
+    print '#'
+    print ' ' * 5
+    print '#'
+    print "\n"
+  end
 
-    def true_line(array)
-      array = array.join('')
-      print array
-      print "\n"
-    end
-    public
-    def board
-      line1
-      true_line($arr[0])
-      line1
-      line2
-      line1
-      true_line($arr[1])
-      line1
-      line2
-      line1
-      true_line($arr[2])
-      line1
-    end
+  def line2
+    print ' '
+    print '# ' * 9
+    print "\n"
+  end
 
-    $arr = [[' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '], [' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ']]
+  def true_line(array)
+    array = array.join('')
+    print array
+    print "\n"
+  end
 
+  public
+
+  def board
+    line1
+    true_line($arr[0])
+    line1
+    line2
+    line1
+    true_line($arr[1])
+    line1
+    line2
+    line1
+    true_line($arr[2])
+    line1
+  end
+
+  $arr = [[' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ']]
 end
 
 class Game
   @@turn = 1
   def turns
-    p "Firt put the a number between 1-3 to select the row then put a number between 1-3 to select the column"
+    p 'Firt put the a number between 1-3 to select the row then put a number between 1-3 to select the column'
     while @@turn < 9
-      if @@turn.odd?
-        flag = "x"
-      else
-        flag = "o"
-      end
+      flag = if @@turn.odd?
+               'x'
+             else
+               'o'
+             end
       p "player #{flag}"
       p 'Select row'
       inp1 = gets
@@ -58,32 +61,32 @@ class Game
       inp2_i = inp2.to_i
       parameters(inp1_i)
       parameters(inp2_i)
-      inp1_i = inp1_i - 1
+      inp1_i -= 1
       inp2_i = (inp2_i * 6) - 3
       $arr[inp1_i][inp2_i] = flag
       board_1 = Board.new
       board_1.board
       @@turn += 1
-      end
     end
+  end
 
-    def parameters(arg)
-      if arg == 1 || arg == 2 || arg == 3
+  def parameters(arg)
+    if arg == 1 || arg == 2 || arg == 3
 
-      else
-        p "Error"
-        turns
-      end
+    else
+      p 'Error'
+      turns
     end
+  end
 end
 
-puts "TIC-TAC-TOE GAME"
-puts "Do you want to play? (yes/no)"
+puts 'TIC-TAC-TOE GAME'
+puts 'Do you want to play? (yes/no)'
 
 want_to_play = gets.chomp.downcase
-if want_to_play == "yes" || want_to_play == "y"
-    game_1 = Game.new
-    game_1.turns
+if want_to_play == 'yes' || want_to_play == 'y'
+  game_1 = Game.new
+  game_1.turns
 else
-puts "THANK YOU, GOOD LUCK!!"
+  puts 'THANK YOU, GOOD LUCK!!'
 end
