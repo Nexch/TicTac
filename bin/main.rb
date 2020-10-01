794d1920052332dd35eab59ed6dffc065ee5c238
Nexch <mauricio.t.1999@gmail.com>
2020-10-01 04:33:17 -0400

    finish the second part of the project

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,13 @@
+# rubocop:disable Metrics/MethodLength
 # rubocop:disable Style/GlobalVars
 # rubocop:disable Style/ClassVars
 # rubocop:disable Style/MultipleComparison
 # rubocop:disable Naming/VariableNumber
 # rubocop:disable Layout/LineLength
-# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Style/GuardClause
 # !/usr/bin/env ruby
 class Board
   private
@@ -51,14 +55,14 @@ end
 class Game
   @@turn = 1
   def turns
-    puts 'Firt put the a number between 1-3 to select the row then put a number between 1-3 to select the column'
+    p 'Firt put the a number between 1-3 to select the row then put a number between 1-3 to select the column'
     while @@turn <= 9
-      flag = if @@turn.odd?
-               'x'
-             else
-               'o'
-             end
-      p "player #{flag}"
+      @@flag = if @@turn.odd?
+                 'x'
+               else
+                 'o'
+               end
+      p "player #{@@flag}"
       p 'Select row'
       inp1 = gets
       p 'Select colum'
@@ -71,11 +75,15 @@ class Game
       inp2_i = (inp2_i * 6) - 3
       same = Logic.new
       same.same_place(inp1_i, inp2_i)
-      $arr[inp1_i][inp2_i] = flag
+      $arr[inp1_i][inp2_i] = @@flag
       board_1 = Board.new
       board_1.board
+      cheking = Logic.new
+      cheking.check if @@turn >= 5
       @@turn += 1
     end
+    p 'draw'
+    play
   end
 
   def parameters(arg)
@@ -86,20 +94,75 @@ class Game
       turns
     end
   end
+
+  def win
+    p "Player #{@@flag} win"
+    play
+  end
+
+  def play
+    @@turn = 1
+    $arr = [[' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ']]
+    start
+  end
+end
+
+class Logic
+  def same_place(argument_x, argument_y)
+    if $arr[argument_x][argument_y] != ' '
+      puts 'You are cheating'
+      turn_again = Game.new
+      turn_again.turns
+    end
+  end
+
+  def check
+    winning = Game.new
+    if $arr[1][9] == $arr[0][15] && $arr[1][9] == $arr[2][3] && $arr[1][19] != ' '
+      winning.win
+    elsif $arr[1][9] == $arr[0][3] && $arr[1][9] == $arr[2][15] && $arr[1][19] != ' '
+      winning.win
+    elsif $arr[1][9] == $arr[0][9] && $arr[1][9] == $arr[2][9] && $arr[1][9] != ' '
+      winning.win
+    elsif $arr[1][9] == $arr[1][15] && $arr[1][9] == $arr[1][3] && $arr[1][9] != ' '
+      winnig.win
+    elsif $arr[0][3] == $arr[0][9] && $arr[0][3] == $arr[0][15] && $arr[0][3] != ' '
+      winning.win
+    elsif $arr[0][3] == $arr[1][3] && $arr[0][3] == $arr[2][3] && $arr[0][3] != ' '
+      winnig.win
+    elsif $arr[2][15] == $arr[2][9] && $arr[2][15] == $arr[2][3] && $arr[2][15] != ' '
+      winning.win
+    elsif $arr[2][15] == $arr[1][15] && $arr[2][15] == $arr[0][15] && $arr[2][15] != ' '
+      winning.win
+    end
+  end
 end
 
 puts 'TIC-TAC-TOE GAME'
-puts 'Do you want to play? (yes/no)'
 
-want_to_play = gets.chomp.downcase
-if want_to_play == 'yes' || want_to_play == 'y'
-  game_1 = Game.new
-  game_1.turns
-else
-  puts 'THANK YOU, GOOD LUCK!!'
+def start
+  puts 'Do you want to play? (yes/no)'
+  want_to_play = gets.chomp.downcase
+  if want_to_play == 'yes' || want_to_play == 'y'
+    board_1 = Board.new
+    board_1.board
+    game_1 = Game.new
+    game_1.turns
+  else
+    puts 'THANK YOU, GOOD LUCK!!'
+    exit
+  end
 end
-# rubocop:enable Style/GlobalVars, Metrics/MethodLength
+
+start
+
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Style/GlobalVars
 # rubocop:enable Style/ClassVars
 # rubocop:enable Style/MultipleComparison
 # rubocop:enable Naming/VariableNumber
 # rubocop:enable Layout/LineLength
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Style/GuardClause
