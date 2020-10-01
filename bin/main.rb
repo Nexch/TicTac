b711bfac1a241a7540509b6f92786c432b0107e8
BrianSammit <briansammit.cruz@gmail.com>
2020-10-01 17:11:29 -0500

    fixed merged problems

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,8 @@
-# rubocop:disable Metrics/MethodLength
 # rubocop:disable Style/GlobalVars
-# rubocop:disable Style/ClassVars
 # rubocop:disable Style/MultipleComparison
 # rubocop:disable Naming/VariableNumber
 # rubocop:disable Layout/LineLength
+
 # !/usr/bin/env ruby
 
 require_relative '../lib/logic_game.rb'
@@ -50,62 +49,6 @@ class Board
   $arr = [[' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ']]
 end
 
-# Game
-class Game
-  @@turn = 1
-  def turns
-    p 'Firt put the a number between 1-3 to select the row then put a number between 1-3 to select the column'
-    while @@turn <= 9
-      @@flag = if @@turn.odd?
-                 'x'
-               else
-                 'o'
-               end
-      p "player #{@@flag}"
-      p 'Select row'
-      input_1 = gets
-      p 'Select colum'
-      input_2 = gets
-      inp1_i = input_1.to_i
-      inp2_i = input_2.to_i
-      parameters(inp1_i)
-      parameters(inp2_i)
-      inp1_i -= 1
-      inp2_i = (inp2_i * 6) - 3
-      same = Logic.new
-      same.same_place(inp1_i, inp2_i)
-      $arr[inp1_i][inp2_i] = @@flag
-      board_1 = Board.new
-      board_1.board
-      cheking = Logic.new
-      cheking.check if @@turn >= 5
-      @@turn += 1
-    end
-    p 'draw'
-    play
-  end
-
-  def parameters(arg)
-    if arg == 1 || arg == 2 || arg == 3
-
-    else
-      p 'Error'
-      turns
-    end
-  end
-
-  def win
-    p "Player #{@@flag} win"
-    play
-  end
-
-  def play
-    @@turn = 1
-    $arr = [[' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ']]
-    start
-  end
-end
-
 puts 'TIC-TAC-TOE GAME'
 
 def start
@@ -127,9 +70,8 @@ board_1 = Board.new
 board_1.board
 first_start.turns
 
-# rubocop:enable Metrics/MethodLength
 # rubocop:enable Style/GlobalVars
-# rubocop:enable Style/ClassVars
+
 # rubocop:enable Style/MultipleComparison
 # rubocop:enable Naming/VariableNumber
 # rubocop:enable Layout/LineLength
